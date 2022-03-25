94d30c0aa9dee22a8257062f85ae8ad6ae68e058
reinier <reinierstribos@gmail.com>
2022-03-25 14:13:24 +0100

    Added ResponseEntities

--- backend/src/main/java/nl/cofano/calculator/services/ValidationService.java
+++ backend/src/main/java/nl/cofano/calculator/services/ValidationService.java
@@ -2,6 +2,8 @@ package nl.cofano.calculator.services;
 
 import nl.cofano.calculator.domain.Ticket;
 import nl.cofano.calculator.xmlresult.TicketResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +14,7 @@ import java.util.stream.Collectors;
 @Service
 public class ValidationService {
 
-    public boolean validate(int user_id, int ticket_id) {
+    public ResponseEntity<Boolean> validate(int user_id, int ticket_id) {
         URI uri = null;
         try {
             uri = new URI("http://localhost:8080/tickets/" + ticket_id);
@@ -23,6 +25,10 @@ public class ValidationService {
         RestTemplate rest = new RestTemplate();
         Ticket ticket = rest.getForObject(uri, Ticket.class);
 
-        return ticket.getUser_id() == user_id;
+        if (ticket == null) {
+            return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<> (ticket.getUser_id() == user_id, HttpStatus.OK);
+        }
     }
 }
