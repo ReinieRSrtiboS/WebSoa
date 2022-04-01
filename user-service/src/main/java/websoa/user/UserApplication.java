844b4de1462181502b81a311b03baf4b2ebb480a
reinier <reinierstribos@gmail.com>
2022-04-01 12:19:27 +0200

    Small bug fix

--- user-service/src/main/java/websoa/user/UserApplication.java
+++ user-service/src/main/java/websoa/user/UserApplication.java
@@ -3,6 +3,7 @@ package websoa.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class UserApplication {
         public static void main(String[] args) {
             SpringApplication.run(UserApplication.class, args);
