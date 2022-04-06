53c7031ffcb54d0b29ed5f2f9811423d3bde64a4
reinier <reinierstribos@gmail.com>
2022-04-06 13:40:29 +0200

    First ugly page for buying tickets

--- event-service/src/main/java/websoa/event/EventController.java
+++ event-service/src/main/java/websoa/event/EventController.java
@@ -6,10 +6,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 import websoa.event.daos.Event;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+import websoa.event.daos.TicketInfo;
 
 import java.io.StringWriter;
 import java.util.Collection;
@@ -45,8 +47,22 @@ public class EventController {
     StringWriter writer = new StringWriter();
     Context context = new Context();
 
-        context.setVariable("events", this.registry.events());
-        template.process("show-events", context, writer);
+    context.setVariable("events", this.registry.events());
+    template.process("show-events", context, writer);
+    return writer.toString();
+    }
+
+    @GetMapping("/order/{event_id}")
+    public String order(@PathVariable String event_id) {
+        StringWriter writer = new StringWriter();
+        Context context = new Context();
+
+        RestTemplate rest = new RestTemplate();
+        TicketInfo[] result = rest.getForObject("http://ticket-service/tickets/" + event_id, TicketInfo[].class);
+//        int[] result = {1, 2, 3};
+
+        context.setVariable("tickets", result.length);
+        template.process("buy-tickets", context, writer);
         return writer.toString();
     }
 }
---
+++ event-service/src/main/java/websoa/event/daos/TicketInfo.java
@@ -0,0 +1,11 @@
+package websoa.event.daos;
+
+public class TicketInfo {
+    public String id;
+    public String event_id;
+    public String user_id;
+    public boolean bought;
+    public boolean activated;
+    public float price;
+}
+
