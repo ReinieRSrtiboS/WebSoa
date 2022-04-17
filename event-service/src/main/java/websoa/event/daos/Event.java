8b55cd66da7b1ac02c661e57bd05ed148d4a163e
Sven Mol <s.mol@student.utwente.nl>
2022-04-17 13:13:58 +0200

    Replace constructor for Event dao

--- event-service/src/main/java/websoa/event/daos/Event.java
+++ event-service/src/main/java/websoa/event/daos/Event.java
@@ -1,15 +1,11 @@
 package websoa.event.daos;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Event {
     public String id;
     public String name;
     public float price;
     public int tickets;
-
-    public Event(String id, String name, float price, int tickets) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.tickets = tickets;
-    }
 }
