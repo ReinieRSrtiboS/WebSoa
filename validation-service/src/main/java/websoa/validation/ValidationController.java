e2dc4084ca48c2e120caffaced2813eb165d81cc
Sven Mol <s.mol@student.utwente.nl>
2022-04-22 16:14:46 +0200

    Fix in validation service

--- validation-service/src/main/java/websoa/validation/ValidationController.java
+++ validation-service/src/main/java/websoa/validation/ValidationController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
@@ -22,16 +19,12 @@ public class ValidationController {
     @Autowired
     private ValidationRegistry registry;
 
-    @GetMapping("/validate")
+    @PostMapping("/validate")
     public String validate(@RequestParam String ticket_id, @RequestParam String user_id) {
         ResponseEntity<HttpStatus> answer = registry.validate(ticket_id, user_id);
         StringWriter writer = new StringWriter();
         Context context = new Context();
-        if (answer.getStatusCode().is2xxSuccessful()) {
-            context.setVariable("success", true);
-        } else {
-            context.setVariable("success", false);
-        }
+        context.setVariable("success", answer.getStatusCode().is2xxSuccessful());
         template.process("validate", context, writer);
         return writer.toString();
     }
